0d6b6285d9b8bb6e4fe4605328cf63dbabaa106b
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-05 15:59:49 +0800

    Added admin url

--- onekloud/urls.py
+++ onekloud/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('apps.events.urls', namespace='events')),
-    url(r'^auth/', include('apps.accounts.urls', namespace='accounts')),
-    url(r'^customers/', include('apps.customers.urls', namespace='customers')),
-    url(r'^companies/', include('apps.companies.urls', namespace='companies')),
-    url(r'^reports/', include('apps.reports.urls', namespace='reports')),
+    (r'^admin/', include(admin.site.urls)),
 )
