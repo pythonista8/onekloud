30fe8db621e9a3da9260ebbed6a873f0045196c0
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-05 15:39:20 +0800

    Changed settings and added apps

---
+++ onekloud/apps/accounts/models.py
@@ -0,0 +1,96 @@
+import warnings
+
+from django.db import models
+from django.utils import timezone
+from django.core.mail import send_mail
+from django.core.exceptions import ImproperlyConfigured
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        PermissionsMixin,
+                                        BaseUserManager,
+                                        SiteProfileNotAvailable)
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, is_superuser, **extra_fields):
+        """Creates and saves a User with the given email and password."""
+        now = timezone.now()
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_created=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_absolute_url(self):
+        return "/users/%s/" % self.pk
+
+    def get_full_name(self):
+        """Returns the first_name plus the last_name, with a space in
+        between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Returns the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        """Sends an email to this User."""
+        send_mail(subject, message, from_email, [self.email])
+
+    def get_profile(self):
+        """Returns site-specific profile for this user. Raises
+        SiteProfileNotAvailable if this site does not allow profiles.
+        """
+        warnings.warn("The use of AUTH_PROFILE_MODULE to define user profiles "
+                      "has been deprecated.", DeprecationWarning, stacklevel=2)
+        if not hasattr(self, '_profile_cache'):
+            from django.conf import settings
+            if not getattr(settings, 'AUTH_PROFILE_MODULE', False):
+                raise SiteProfileNotAvailable(
+                    'You need to set AUTH_PROFILE_MODULE in your project '
+                    'settings')
+            try:
+                app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
+            except ValueError:
+                raise SiteProfileNotAvailable(
+                    'app_label and model_name should be separated by a dot in '
+                    'the AUTH_PROFILE_MODULE setting')
+            try:
+                model = models.get_model(app_label, model_name)
+                if model is None:
+                    raise SiteProfileNotAvailable(
+                        'Unable to load the profile model, check '
+                        'AUTH_PROFILE_MODULE in your project settings')
+                self._profile_cache = model._default_manager.using(
+                    self._state.db).get(user__id__exact=self.id)
+                self._profile_cache.user = self
+            except (ImportError, ImproperlyConfigured):
+                raise SiteProfileNotAvailable
+        return self._profile_cache
---
+++ onekloud/settings.py
@@ -0,0 +1,157 @@
+"""
+Django settings for Onekloud project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(__file__)
+
+# Adding system paths
+import sys
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '9gfi8y+so)y+51=b@xpfnnlkm9^g+#xgjnl%sycs_n!5y)pvnz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'onekloud.com'
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
+# Django application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+
+# Third-party application definition
+INSTALLED_APPS += (
+    'south',
+)
+
+
+# Onekloud application definition
+INSTALLED_APPS += (
+    'onekloud.apps.accounts',
+)
+
+
+# Django middleware classes
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+# Django context processors
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.media',
+    'django.contrib.messages.context_processors.messages',
+)
+
+
+ROOT_URLCONF = 'onekloud.urls'
+
+WSGI_APPLICATION = 'onekloud.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onekloud',
+        'USER': 'oneklouduser',
+        'PASSWORD': 'vivendi89',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kuala_Lumpur'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'common_static'),
+)
+
+
+# Template directories
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, '../logs/error.log')
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True
+        }
+    }
+}
+
+
+# Dev settings
+try:
+    from onekloud.settings_local import *
+except ImportError:
+    pass
+
+
+# Test settings
+if 'test' in sys.argv:
+    from onekloud.settings_test import *
