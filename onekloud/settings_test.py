33c1a0245d8245c4bd982ae2fe3939f33d851c58
Aldash <aldash@easyuni.com>
2014-01-08 14:42:44 +0800

    Test db and html

--- onekloud/settings_test.py
+++ onekloud/settings_test.py
@@ -1,5 +1,21 @@
 # Test settings
 
+import os
+BASE_DIR = os.path.dirname(__file__)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
+
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
