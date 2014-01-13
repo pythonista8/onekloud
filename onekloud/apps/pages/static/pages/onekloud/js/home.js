264777d55434185d7dc5945002114627ae110884
Aldash <aldash@easyuni.com>
2014-01-13 15:08:38 +0800

    Sign up button animation

--- onekloud/apps/pages/static/pages/onekloud/js/home.js
+++ onekloud/apps/pages/static/pages/onekloud/js/home.js
@@ -1,3 +1,26 @@
 $('#learn-more').on('click', function() {
   scrollToAnchor('content');
 });
+
+$('#signup .pure-button').on('click', function(event) {
+  event.preventDefault;
+
+  scrollToAnchor('showcase', function() {
+    $('.fade').hide().css('display', 'block').fadeIn();
+
+    var signupContainer = $('#signup-form-container');
+
+    signupContainer.animate({
+      background: '#202021'
+    }, 1000, 'easeOutQuart', function() {
+      setTimeout(function() {
+        $('.fade').fadeOut();
+        signupContainer.animate({
+          background: 'transparent'
+        });
+      }, 3000);
+    });
+  });
+
+  return false;
+});
--- onekloud/common_static/js/onekloud/lib.js
+++ onekloud/common_static/js/onekloud/lib.js
@@ -1,11 +1,16 @@
 // Smoothly scroll browser window to the anchor. Pass element's id
 // without `#`.
-function scrollToAnchor(id){
-  var anchor = $('#' + id);
-  $('html, body').animate({
-    scrollTop: anchor.offset().top
-  }, {
+function scrollToAnchor(id, fn) {
+  var opts = {
     duration: 1000,
     easing: 'easeOutQuart'
-  });
+  }
+
+  if (fn !== undefined) opts['complete'] = fn;
+
+  var anchor = $('#' + id);
+
+  $('html, body').animate({
+    scrollTop: anchor.offset().top
+  }, opts);
 }
