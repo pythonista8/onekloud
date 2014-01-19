6a9ec7e9791a2d5ce2b0bec9ad5dbbf0087594f5
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-19 23:43:16 +0800

    Updated ToS and email lib.

--- onekloud/lib/email.py
+++ onekloud/lib/email.py
@@ -6,11 +6,10 @@ from django.utils.safestring import mark_safe
 def send_invitation(email_list):
     subject = "Welcome to Onekloud CRM!"
     from_ = 'support@onekloud.com'
-    bcc = ('aldash@onekloud.com', 'samantha@onekloud.com')
     html = mark_safe(render_to_string('onekloud/email/welcome.html'))
 
     for email in email_list:
-        msg = EmailMessage(subject, html, from_, [email], bcc,
+        msg = EmailMessage(subject, html, from_, [email],
                            headers={'Reply-To': from_})
         msg.content_subtype = 'html'
         msg.send(fail_silently=True)
