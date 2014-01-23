24edd42682db4e308bc6f4a92be9ed617bfb5eb6
Aldash <aldash@easyuni.com>
2014-01-23 14:35:44 +0800

    Catch error and print

--- onekloud/lib/email.py
+++ onekloud/lib/email.py
@@ -17,7 +17,7 @@ def send_invitation(email_list):
             i=i + 1, total=len(email_list), email=email))
         try:
             msg.send(fail_silently=False)
-        except SMTPException:
-            print("Failed")
+        except SMTPException, err:
+            print(err)
         else:
             print("Sent")
