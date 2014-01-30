478fa79a7d91eeebf89a82999355095c87d7b37c
Aldash <aldash@easyuni.com>
2014-01-30 14:41:08 +0800

    Set maximum number of emails

--- onekloud/lib/email.py
+++ onekloud/lib/email.py
@@ -5,9 +5,15 @@ from django.utils.safestring import mark_safe
 
 
 def send_invitation(email_list):
+    MAX_NUMBER = 50
+
     subject = "Welcome to Onekloud CRM!"
     from_ = 'support@onekloud.com'
     html = mark_safe(render_to_string('onekloud/email/welcome.html'))
+
+    if len(email_list) > MAX_NUMBER:
+        raise ValueError(
+            "Number of emails must not exceed {}.".format(MAX_NUMBER))
 
     for i, email in enumerate(email_list):
         msg = EmailMessage(subject, html, from_, [email],
