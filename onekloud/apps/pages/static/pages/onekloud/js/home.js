8129cf9c990ac7f6403be844e74e80a354096f01
Aldash Biybosunov <pythonista8@gmail.com>
2014-02-06 23:43:18 +0800

    Mobile-friendly signup modal

--- onekloud/apps/pages/static/pages/onekloud/js/home.js
+++ onekloud/apps/pages/static/pages/onekloud/js/home.js
@@ -2,30 +2,3 @@ $('#learn-more').on('click', function() {
   scrollToAnchor('content');
   return false;
 });
-
-$('#signup .pure-button').on('click', function() {
-  scrollToAnchor('showcase', function() {
-    var signupContainer = $('#signup-form-container');
-
-    $('.fade').fadeIn(250);
-    signupContainer.find('input[name="email"]').focus();
-
-    signupContainer.css('border-radius', 5).animate({
-      backgroundColor: '#202021'
-    }, 250, 'easeOutQuart', function() {
-
-      setTimeout(function() {
-        $('.fade').fadeOut(250);
-
-        signupContainer.animate({
-          backgroundColor: 'transparent'
-        }, 250, 'easeOutQuart', function() {
-          signupContainer.css('border-radius', 0);
-        });
-      }, 3000);
-
-    });
-  });
-
-  return false;
-});
